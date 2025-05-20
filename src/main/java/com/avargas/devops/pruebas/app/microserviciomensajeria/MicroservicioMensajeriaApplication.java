b31761f3c29c203df5225b69b2f7baa63bf37775
AnaVargasJ <anamariavargasj1590@gmail.com>
2025-05-19 22:13:45 -0500

    ADD: Creacion del proyecto

---
+++ src/main/java/com/avargas/devops/pruebas/app/microserviciomensajeria/MicroservicioMensajeriaApplication.java
@@ -0,0 +1,13 @@
+package com.avargas.devops.pruebas.app.microserviciomensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMensajeriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioMensajeriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avargas/devops/pruebas/app/microserviciomensajeria/MicroservicioMensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avargas.devops.pruebas.app.microserviciomensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMensajeriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
